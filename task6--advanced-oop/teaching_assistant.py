d38c6ca5346750c50c550070bcd1c4e2d0fdb0ed
Mbabazidinah <mbabazidinah90@gmail.com>
2025-05-09 18:07:00 +0400

    Delete task6--advanced-oop directory

--- task6--advanced-oop/teaching_assistant.py
+++
@@ -1,9 +0,0 @@
-from student import Student
-from instructor import Instructor
-
-class TeachingAssistant(Student, Instructor):
-    def __init__(self, name):
-        super().__init__(name)
-
-    def get_role(self):
-        return "TA"
